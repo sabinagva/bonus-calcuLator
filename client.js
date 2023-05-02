da444880bd0a0f09a8b40c64d5dc74888e6232bc
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-02 15:58:26 -0500

    completed till if statement

--- client.js
+++ client.js
@@ -32,8 +32,83 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
+// - Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+// and should receive an additional 5%.
+// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// - No bonus can be above 13% or below 0% total.
 
+
+
+
+
+
+
+  
+
+
+
+
+
+//This loops over the employee array and console logs the information. 
+
+function calculateIndividualEmployeeBonus (employee) {
+  let name = employee.name
+  console.log(name)
+  let bonusPercentage = 0;
+  
+    if (employee.reviewRating <= 2) {
+    bonusPercentage = 0;
+  }
+    if (employee.reviewRating === 3) {
+    bonusPercentage = (employee.annualSalary * 4)/100;
+    
+  }
+   if (employee.reviewRating === 4) {
+    bonusPercentage = (employee.annualSalary * 6)/100;
+    
+  }
+   if (employee.reviewRating === 5) {
+    bonusPercentage = (employee.annualSalary * 10)/100;
+    
+  }
+   if (employee.employeeNumber.length === 4) {
+    bonusPercentage = (bonusPercentage *5)/100;
+  }
+   if (employee.annualSalary > 65000) {
+    bonusPercentage = (bonusPercentage - 1)/100
+  }
+  
+  return {name: employee.name,
+    bonusPercentage: 117,
+    totalCompensation: 70000,
+    totalBonus: 1
+}
+}
+  
+   
+   
+for (let employee of employees) {
+  console.log("This is the info loop:", employee); 
+ console.log(calculateIndividualEmployeeBonus(employee));
+
+}
+
+
+
+ 
+
+
+
+// Write a declared function that takes in one **Employee** object (as an argument to the function), and `return` a new **object** with the following properties. _Note these properties are different than the ones you start with!_
+
+// * The `name` property should contain the employee's name.
+// * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -48,10 +123,9 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+
   // your logic here
   
   
   // return new object with bonus results
 
-}
